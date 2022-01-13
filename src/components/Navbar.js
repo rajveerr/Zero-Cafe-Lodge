586a55e2632ce55367d0763f39bb49c620c12e3d
Rajveer Singh <rajveer@Rajveers-MacBook-Pro.local>
2022-01-13 12:19:25 +0530

    Updating Logo File

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import logo from "../img/logo-zero-cafe-lodge.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
